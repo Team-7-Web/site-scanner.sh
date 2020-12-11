25acbcb0ca4efc3b61d54e641ae6405e28148e16
10thdiv <71663974+10thdiv@users.noreply.github.com>
2020-12-10 17:31:09 -0800

    Update site-scanner.sh
    Final version for report pre-view

--- site-scanner.sh
+++ site-scanner.sh
@@ -58,7 +58,8 @@ rm -vf nmap7.txt
 rm -vf nikto7.txt
 rm -vf nikto7temp.txt
 rm -vf whatweb7.txt
-rm -vf team7scan.txt
+rm -vf wpscan7.txt
+rm -vf wpscan7temp.txt
 rm -vf $URL.txt
 
 
@@ -67,31 +68,32 @@ rm -vf $URL.txt
 #whatweb tool
 echo -e ${RED}"Now running step #1 WhatWeb Tool Scan"${CYN}
 whatweb -v -a 3 "$URL" --log-verbose=whatweb7.txt
-
+echo
 
 #dirb tool
 echo -e ${RED}"Now running step #2 Dir Buster Scan"${CYN}
 dirb http://"$URL" -w -f -N 404 -o dirb7.txt
-
+echo
 
 #nikto tool
 echo -e ${RED}"Now running step #3 Nikto Scan"${CYN}
 nikto -h "$URL" -Display 3 -output nikto7.txt
-
+echo
 
 #nmap tool
 echo -e ${RED}"Now running step #4 Nmap Scan"${CYN}
 nmap -n -v -A -T4 -p- "$URL" -o nmap7.txt
+echo
 
-
+#wpscan tool
+echo -e ${RED}"Now running step #5 WPScan"${CYN}
+wpscan --url http://"$URL" -v --detection-mode aggressive --rua --ignore-main-redirect -o wpscan7.txt
 echo -e ${NC}
+echo
 
 #Scan complete message
 echo -e ${RED}"SCAN COMPLETE!"
-
 echo
-echo
-
 
 #Editing individual reports.
 
@@ -106,30 +108,50 @@ sed -i 's|---- Entering directory: http://'"$URL"'||g' dirb7temp.txt
 #Nikto results processing
 tail -n +3 nikto7.txt > nikto7temp.txt
 
+#WPScan results processing
+tail -n +14 wpscan7.txt > wpscan7temp.txt
+
 
 #Consolidating logs into final report
 touch $URL.txt
+echo  >> $URL.txt
 echo "Nmap Report *********************************" >> $URL.txt
+echo  >> $URL.txt
 echo  >> $URL.txt
 cat nmap7.txt >> $URL.txt
 echo  >> $URL.txt
 echo  >> $URL.txt
+echo  >> $URL.txt
 echo "Dir Buster Report *********************************" >> $URL.txt
+echo  >> $URL.txt
 echo  >> $URL.txt
 cat dirb7temp.txt >> $URL.txt
 echo  >> $URL.txt
 echo  >> $URL.txt
+echo  >> $URL.txt
 echo "WhatWeb Report *********************************" >> $URL.txt
+echo  >> $URL.txt
 echo  >> $URL.txt
 cat whatweb7.txt >> $URL.txt
 echo  >> $URL.txt
 echo  >> $URL.txt
 echo "Nikto Report *********************************" >> $URL.txt
 echo  >> $URL.txt
+echo  >> $URL.txt
 cat nikto7temp.txt >> $URL.txt
 echo  >> $URL.txt
+echo  >> $URL.txt
+echo  >> $URL.txt
+echo "WPScan Report *********************************" >> $URL.txt
+cat wpscan7temp.txt >> $URL.txt
+echo  >> $URL.txt
+
+
+
 echo "End of Report *********************************" >> $URL.txt
 
+#additional formatting lines
+echo
 
 #File clean up
 rm -vf dirb7.txt
@@ -138,15 +160,21 @@ rm -vf nmap7.txt
 rm -vf nikto7.txt
 rm -vf nikto7temp.txt
 rm -vf whatweb7.txt
+rm -vf wpscan7.txt
+rm -vf wpscan7temp.txt
+
+#additional formatting lines
+echo
+echo
 
 #Completion message and final report name
 echo -e ${RED}"All processing is done."
 echo "Report Completed!"
+echo
 echo -e "Filename: $URL.txt"${NC}
+echo
 #Completion timer message but not currently working
 duration=$(( SECONDS - start ))
-
-
 
 
 
